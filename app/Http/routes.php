134a16e18a40af492947f09fbc2bb030095640c1
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-04-07 06:38:42 +0200

    Adds Comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,26 +17,35 @@ Route::get('/', function () {
 
 Route::auth();
 
+//edit user profile
 Route::get('user/edit/{id}', 'UController@edit')->name('user.edit');
 Route::post('user/edit/{id}', 'UController@update')->name('user.edit');
 
 Route::get('/home', 'HomeController@index');
 
+// select pokemon
 Route::get('user/pokemon/{id}', 'UController@select')->name('user.pokemon');
 Route::post('user/pokemon/{id}', 'UController@save')->name('user.pokemon');
 
+// show my pokemons
 Route::get('user/mypokemons/{id}', 'UController@mypo')->name('user.mypokemons');
 
+//adandon pokemon
 Route::get('user/mydelete/{id}', 'UController@abandon')->name('user.mydelete');
 
+// sum of the strengths
 Route::get('user/strength/{id}', 'UController@myStrength')->name('user.strength');
 
+// show all pokemon
 Route::get('pokemon/show', 'PokemonController@show')->name('pokemon.show');
 
+// add pokemon
 Route::get('pokemon/add', 'PokemonController@create')->name('pokemon.add');
 Route::post('pokemon/add', 'PokemonController@store')->name('pokemon.add');
 
+// edit pokemon
 Route::get('pokemon/edit/{id}', 'PokemonController@edit')->name('pokemon.edit');
 Route::post('pokemon/edit/{id}', 'PokemonController@update')->name('pokemon.edit');
 
+// delete pokemon
 Route::get('pokemon/delete/{id}', 'PokemonController@delete')->name('pokemon.delete');
